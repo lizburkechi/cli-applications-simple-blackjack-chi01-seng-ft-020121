5a84ec43bb814152bbb7d5bbb5015a19efe9ae30
Liz Burke <liz.burke.chi@gmail.com>
2021-01-28 18:54:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,17 +1,17 @@
 def welcome
-  # code #welcome here
+  "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total_num)
+  "Your cards add up to #{card_total_num}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  "Type 'h' to hit or 's' to stay".
 end
 
 def get_user_input
@@ -38,7 +38,6 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner
+#def runner
   # code runner here
-end
-    
+#end
